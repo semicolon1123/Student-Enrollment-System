db8fdfe4905f9d76b96aa21d8aa020fb84804dd5
semicolon1123 <placinorenson1123@gmail.com>
2022-12-08 02:07:23 +0800

    Creating JFrame Form for StudentProfile and Creating new Database Tables and assigning Primary and Foreign keys

--- src/MySQLConnect.java
+++ src/MySQLConnect.java
@@ -12,8 +12,8 @@ public class MySQLConnect {
             Class.forName("com.mysql.cj.jdbc.Driver");
             
             // Establishing a Connection
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp_db","root","123");
-            // JOptionPane.showMessageDialog(null, "Connection Successful");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/stud_db","root","123");
+            //JOptionPane.showMessageDialog(null, "Connection Successful");
             return conn;
         } catch (Exception e){
             JOptionPane.showMessageDialog(null, e);
